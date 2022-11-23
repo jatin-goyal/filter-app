8b8d115ded3090655754df1a0ba076a349c4f0c9
Jatin Goyal <25JG2001@GMAIL.COM>
2022-11-23 19:18:30 +0530

    filter app with one filter made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Filter from "./Filter";
+import Movie from "./Movie";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=82a9bcad03c06a220c1bbecf97b0c6d0&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <div className="popular_movies">
+        {filtered.map((movie) => {
+          return <Movie movie={movie} key={movie.id} />;
+        })}
+      </div>
     </div>
   );
 }
